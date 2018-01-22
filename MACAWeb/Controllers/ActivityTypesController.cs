7190858ea2fae74b702dc2dcdb6c32c064d437b3
Martina Tamášová <3533780827@upjs.sk>
2018-01-22 14:12:38 +0100

    Activity Types

---
+++ MACAWeb/Controllers/ActivityTypesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MACAWeb.Models;
+using Microsoft.AspNet.Identity;
+
+namespace MACAWeb.Controllers
+{
+    [Authorize(Roles = "SuperAdmin")]
+    public class ActivityTypesController : Controller
+    {
+        private ActivityTypeDbContext db = new ActivityTypeDbContext();
+
+        // GET: ActivityTypes
+        public ActionResult Index()
+        {
+            return View(db.ActivityTypes.ToList());
+        }
+
+        // GET: ActivityTypes/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            if (activityType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ActivityTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Description")] ActivityType activityType)
+        {
+            if (ModelState.IsValid)
+            {
+                activityType.ActivityTypeID = Guid.NewGuid();
+
+                activityType.DateCreated = DateTime.Now;
+                activityType.DateModified = activityType.DateCreated;
+
+                activityType.UserCreatedID = Guid.Parse(User.Identity.GetUserId());
+                activityType.UserModifiedID = activityType.UserCreatedID;
+
+                db.ActivityTypes.Add(activityType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(activityType);
+        }
+
+        // GET: ActivityTypes/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            if (activityType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityType);
+        }
+
+        // POST: ActivityTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ActivityTypeID,Name,Description")] ActivityTypeViewModel activityTypeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ActivityType model = db.ActivityTypes.Find(activityTypeViewModel.ActivityTypeID);
+
+                model.Name = activityTypeViewModel.Name;
+                model.Description = activityTypeViewModel.Description;
+
+                model.DateModified = DateTime.Now;
+                model.UserModifiedID = Guid.Parse(User.Identity.GetUserId());
+
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(activityTypeViewModel);
+        }
+
+        // GET: ActivityTypes/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            if (activityType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityType);
+        }
+
+        // POST: ActivityTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            ActivityType activityType = db.ActivityTypes.Find(id);
+            db.ActivityTypes.Remove(activityType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MACAWeb/Models/ActivityTypesModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MACAWeb.Models
+{
+    public class ActivityType
+    {
+        [Key]        
+        public Guid ActivityTypeID { get; set; }
+
+        [Display(Name = "Activity Type Name")]
+        [Required(ErrorMessage = "The name must be specified!")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]   
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Display(Name = "Date Created")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime DateCreated { get; set; }
+
+        [Display(Name = "Date Modified")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime DateModified { get; set; }
+        
+        public Guid UserCreatedID { get; set; }
+
+        public Guid UserModifiedID { get; set; }
+    }
+
+    public class ActivityTypeViewModel
+    {
+        public Guid ActivityTypeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ActivityTypeDbContext : DbContext
+    {
+        public DbSet<ActivityType> ActivityTypes { get; set; }
+
+        public ActivityTypeDbContext() : base("MACA") { }
+    }
+}
