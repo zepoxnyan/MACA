59faf826a28fd5092884bcb2a23222f370d72b5d
Rudolf Bele <rudolf.bele@gmail.com>
2019-07-02 11:35:20 +0200

    Better popupimage tooltip

--- MACAWeb/Scripts/popupimage.js
+++ MACAWeb/Scripts/popupimage.js
@@ -1,7 +1,15 @@
 ﻿$(document).ready(function () {
     $(".popupimage a").mouseover(function () {
         $(".popupimage a img").css("display", "none"); // hide all product images
-        $(this).find("img").css("display", "inline-block"); // show current hover image
+        $(this).find("img").css({
+            //display: "inline-block",
+            "display": "grid",
+            "position": "absolute",
+            "width": "80",
+            "height": "90",
+            "border-radius": "15px",
+            "border": "2px solid #000000"
+        });//show div
     })
     $(".popupimage a").mouseout(function () {
         $(".popupimage a img").css("display", "none"); // hide all product images
